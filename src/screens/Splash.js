6be5c64edea52486236a98ff981227e1cd4f8b4a
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-20 15:35:33 +0700

    finish splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import {View, StyleSheet, Image, Text, Button} from 'react-native';
+import {View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
 
 export default function Splash({navigation}) {
+  function goToHome() {
+    navigation.navigate('Home');
+  }
+
   return (
     <View style={styles.container}>
-      <View>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/logo.png')}
-        />
-        <View style={sliderStyles.container}>
-          <View style={sliderStyles.item}></View>
-          <View style={sliderStyles.item}></View>
-          <View style={sliderStyles.item}></View>
-        </View>
-        <Text style={contentStyles.title}>Pet Near Me</Text>
-        <Text style={contentStyles.description}>
-          Find your favorite pet close to you
-        </Text>
-        <Button style={contentStyles.button}>Get Started</Button>
+      <Image
+        style={styles.image}
+        source={require('../assets/images/logo.png')}
+      />
+      <View style={sliderStyles.container}>
+        <View style={[sliderStyles.item, sliderStyles.itemActive]}></View>
+        <View style={sliderStyles.item}></View>
+        <View style={sliderStyles.item}></View>
       </View>
+      <Text style={contentStyles.title}>Pet Near Me</Text>
+      <Text style={contentStyles.description}>
+        {'Join and discover your favorite\npet in your locality'}
+      </Text>
+      <TouchableOpacity onPress={() => goToHome()}>
+        <Text style={contentStyles.button}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -29,21 +33,54 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
-    width: 100,
-    height: 100,
+    width: 170,
+    height: 170,
+    marginBottom: 20,
     resizeMode: 'cover',
+    alignSelf: 'center',
   },
 });
 
 const sliderStyles = StyleSheet.create({
-  container: {},
-  item: {},
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  item: {
+    width: 25,
+    height: 10,
+    marginEnd: 10,
+    borderRadius: 5,
+    backgroundColor: '#e0e0e0',
+  },
+  itemActive: {
+    backgroundColor: '#4daefe',
+  },
 });
 
 const contentStyles = StyleSheet.create({
-  title: {},
-  description: {},
-  button: {},
+  title: {
+    color: '#333',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  description: {
+    color: '#ababab',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#4daefe',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    color: '#fff',
+  },
 });
