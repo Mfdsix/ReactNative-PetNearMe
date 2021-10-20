0c092975e77d65cc1fade7c4b573af5e40ae950e
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-20 15:35:07 +0700

    add categories data

---
+++ src/datas/categories.js
@@ -0,0 +1,25 @@
+const dogPhoto = require('../assets/images/dog.png');
+const catPhoto = require('../assets/images/cat.png');
+const fishPhoto = require('../assets/images/fish.png');
+const rabbitPhoto = require('../assets/images/rabbit.png');
+
+const categories = [
+  {
+    title: 'Dogs',
+    photo: dogPhoto,
+  },
+  {
+    title: 'Cats',
+    photo: catPhoto,
+  },
+  {
+    title: 'Fish',
+    photo: fishPhoto,
+  },
+  {
+    title: 'Rabbits',
+    photo: rabbitPhoto,
+  },
+];
+
+export default categories;
